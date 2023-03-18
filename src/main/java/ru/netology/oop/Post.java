507cafa6472aaef1d1f745d9f74f244eabf1deb1
Alex <jswift888@gmail.com>
2023-03-18 11:00:06 +0300

    oop 1

---
+++ src/main/java/ru/netology/oop/Post.java
@@ -0,0 +1,12 @@
+package ru.netology.oop;
+
+public class Post {
+    FormDate birthday = new FormDate();
+
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
